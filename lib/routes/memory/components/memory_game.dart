77c1a86fbafba6bf39c52a9060013b197322ca7c
Micha Sengotta <micha.sengotta@gmail.com>
2024-04-28 22:34:07 +0200

    Auto scale memory grid

--- lib/routes/memory/components/memory_game.dart
+++ lib/routes/memory/components/memory_game.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:languages/components/term_widget.dart';
@@ -8,7 +10,12 @@ import 'package:languages/services/local_storage_repository.dart';
 import 'package:text_to_speech/text_to_speech.dart';
 
 class MemoryGame extends StatefulWidget {
-  const MemoryGame({super.key});
+  final int uniqueCardCount;
+
+  const MemoryGame({
+    super.key,
+    this.uniqueCardCount = 8,
+  });
 
   @override
   State<MemoryGame> createState() => _MemoryGameState();
@@ -24,40 +31,54 @@ class _MemoryGameState extends State<MemoryGame> {
     final appModelRepository = GetIt.I<LocalStorageRepository<AppModel>>();
     final appModel = appModelRepository.getOrDefault();
     final shuffledTerms = appModel.terms.toList()..shuffle();
-    _model = MemoryModel(shuffledTerms.take(8).toSet());
+    _model = MemoryModel(shuffledTerms.take(widget.uniqueCardCount).toSet());
   }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    const minCardWidth = 200;
-    final width = MediaQuery.of(context).size.width;
     final textToSpeech = GetIt.I<TextToSpeech>();
 
-    return GridView.count(
-      padding: const EdgeInsets.all(16),
-      crossAxisCount: (width / minCardWidth).floor(),
-      mainAxisSpacing: 8,
-      crossAxisSpacing: 8,
-      children: [
-        for (final card in _model.cards)
-          Card(
-            color: card.isMatched ? theme.colorScheme.primary : null,
-            child: InkWell(
-              onTapUp: (_) {
-                setState(() {
-                  _model.onTapCard(card);
-                });
-                textToSpeech.speak(card.value.target);
-              },
-              child: card.isFaceUp
-                  ? TermWidget(
-                      term: card.value,
-                    )
-                  : null,
-            ),
-          ),
-      ],
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final cardCount = widget.uniqueCardCount * _model.cardCopyCount;
+
+        final area = constraints.maxWidth * constraints.maxHeight;
+        final maxCardArea = area / cardCount;
+        final maxCardSide = sqrt(maxCardArea);
+        final rowCount = (constraints.maxHeight / maxCardSide).ceil();
+        final cardHeight = constraints.maxHeight / rowCount;
+        final columnCount = (constraints.maxWidth / cardHeight).ceil();
+
+        return GridView.count(
+          padding: const EdgeInsets.all(0),
+          crossAxisCount: columnCount,
+          mainAxisSpacing: 0,
+          crossAxisSpacing: 0,
+          childAspectRatio: 1,
+          children: [
+            for (final card in _model.cards)
+              Card(
+                color: card.isMatched ? theme.colorScheme.primary : null,
+                child: InkWell(
+                  onTapUp: (_) {
+                    setState(() {
+                      _model.onTapCard(card);
+                    });
+                    textToSpeech.speak(card.value.target);
+                  },
+                  child: card.isFaceUp
+                      ? FittedBox(
+                          child: TermWidget(
+                            term: card.value,
+                          ),
+                        )
+                      : null,
+                ),
+              ),
+          ],
+        );
+      },
     );
   }
 }
